c7259c76f482b2e913bf0584b747db56a2a0dfae
Félix Vicente <felixvicent1306@gmail.com>
2023-06-19 10:13:32 -0300

    :sparkles: add admin permission to manage users

--- src/main/java/com/felps/api/web/user/UserController.java
+++ src/main/java/com/felps/api/web/user/UserController.java
@@ -9,16 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.felps.api.model.UserAccount;
 import com.felps.api.service.UserService;
 
 @RestController
 @RequestMapping("/users")
+@Secured({ "ROLE_ADMIN" })
 public class UserController {
   @Autowired
   private UserService userService;
@@ -26,7 +29,15 @@ public class UserController {
   @PostMapping
   public ResponseEntity<Object> store(@RequestBody @Valid CreateUserForm form) {
 
-    return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(form));
+    try {
+      UserAccount newUser = userService.createUser(form);
+      return ResponseEntity.status(HttpStatus.CREATED)
+          .body(UserDTO.builder().id(newUser.getId()).name(newUser.getName()).email(newUser.getEmail())
+              .active(newUser.isActive()).build());
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body("User already exists");
+    }
+
   }
 
   @GetMapping
