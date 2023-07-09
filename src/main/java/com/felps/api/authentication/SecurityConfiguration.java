3647d265e3bf3f9d467c906a0f596d0a34083115
Félix Vicente <felixvicent1306@gmail.com>
2023-07-09 15:21:04 -0300

    :wrench: config firewall

--- src/main/java/com/felps/api/authentication/SecurityConfiguration.java
+++ src/main/java/com/felps/api/authentication/SecurityConfiguration.java
@@ -8,9 +8,12 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 import com.felps.api.service.UserService;
 
@@ -49,4 +52,12 @@ public class SecurityConfiguration {
       throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
   }
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    StrictHttpFirewall firewall = new StrictHttpFirewall();
+    firewall.setAllowBackSlash(true);
+    return (web) -> web.httpFirewall(firewall);
+  }
+
 }
