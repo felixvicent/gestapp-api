546c2ee974ed6c7326e8f837e576e222d1767380
Félix Vicente <felixvicent1306@gmail.com>
2024-07-04 22:04:55 -0300

    configure typescript

--- handler.ts
+++ handler.ts
@@ -1,8 +1,10 @@
-export async function handler (event) {
+import type { APIGatewayProxyEventV2 } from "aws-lambda";
+
+export async function hello(event: APIGatewayProxyEventV2) {
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: "Go Serverless v4! Your function executed successfully!",
     }),
   };
-};
+}
